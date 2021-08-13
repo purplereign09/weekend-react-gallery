f8819e4aeefe900788443f6a88570ee32bb39fc4
Patty Baraibar <pattybaraibar@msn.com>
2021-08-12 23:19:13 -0500

    initial commit

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,17 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+
+    { id: 1, path: 'images/office.png', description: 'Photoshop rendering of redesigned office.', likes: 0 },
+
+    { id: 2, path: 'images/fancy_patisserie.png', description: 'Photoshop rendering of bakery commercial interior.', likes: 0 },
+
+    { id: 3, path: 'images/jomalone.png', description: 'Previous rendering inspired by Jo Malone interiors.', likes: 0 },
+
+    { id: 4, path: 'images/kitchen.png', description: 'Kitchen redesign using Adobe Photoshop.', likes: 0 },
+
+    { id: 5, path: 'images/new_apartment.png', description: 'Apartment: re-imagined using Photoshop.', likes: 0 },
+
+    { id: 6, path: 'images/individual_elements.png', description: 'Individual elements used to customize previous images interior.', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
