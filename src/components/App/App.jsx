e6092243677b8ea9ccd9bce66e3b1f9f02c78724
Patty Baraibar <pattybaraibar@msn.com>
2021-08-14 12:11:49 -0500

    added gallery list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,7 +8,12 @@ function App() {
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/office.png"/>
+        <img src="images/fancy_patisserie.png"/>
+        <img src="images/jomalone.png"/>
+        <img src="images/kitchen.png"/>
+        <img src="images/new_apartment.png"/>
+        <img src="images/individual_elements.png"/>
       </div>
     );
 }
